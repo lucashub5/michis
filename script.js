286d8af14a2450ab4eeefa2d214821cb968632f6
lucashub5 <lucascarovano@gmail.com>
2023-12-23 23:25:06 -0300

    3 commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleVisibilityLuni() {
+    var box = document.getElementById('cajaLuni');
+    var cat = document.getElementById('luni');
+    box.style.display = 'none';
+    cat.style.display = 'block';
+  }
+
+function toggleVisibilityMumi() {
+    var box = document.getElementById('cajaMumi');
+    var cat = document.getElementById('mumi');
+    box.style.display = 'none';
+    cat.style.display = 'block';
+  }
+
+function toggleVisibilityGreti() {
+    var box = document.getElementById('cajaGreti');
+    var cat = document.getElementById('greti');
+    box.style.display = 'none';
+    cat.style.display = 'block';
+  }
+
+function toggleVisibilityPichi() {
+    var box = document.getElementById('cajaPichi');
+    var cat = document.getElementById('pichi');
+    box.style.display = 'none';
+    cat.style.display = 'block';
+  }
